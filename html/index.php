12f5aa25bf45d0f4016b3ecc520ada307d538e1d
genzouw <genzouw@gmail.com>
2019-09-27 01:52:22 +0900

    Use from base64 to sha256 to convert url to file path.

--- html/index.php
+++ html/index.php
@@ -3,10 +3,10 @@
 $url = $_GET['url'];
 
 if (parse_url($url)) {
-    $base64Url = base64_encode($url);
+    $sha256Url = hash_file('sha256', $url);
 
     header('Content-Type: text/plain;charset=UTF-8');
-    $shell = 'cat /var/www/db/' . $base64Url . ' || curl -sS "' . $url . '" | /usr/lib/x86_64-linux-gnu/go/bin/ansize /dev/stdin /var/www/db/' . $base64Url . ';';
+    $shell = 'cat /var/www/db/' . $sha256Url . ' || curl -sS "' . $url . '" | /usr/lib/x86_64-linux-gnu/go/bin/ansize /dev/stdin /var/www/db/' . $sha256Url . ';';
     exec($shell, $output, $returnValue);
 
     foreach ($output as $row) {
