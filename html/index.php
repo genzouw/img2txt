35cf94b08636a89bdc60f4ef6cd89aacbcd456b4
genzouw <genzouw@gmail.com>
2019-09-27 12:40:51 +0900

    上下左右からトリミングする機能を追加.

--- html/index.php
+++ html/index.php
@@ -3,10 +3,20 @@
 $url = $_GET['url'];
 
 if (parse_url($url)) {
+    $trimLeft = $_GET['tl'] ?: 0;
+    $trimRight = $_GET['tr'] ?: 0;
+    $trimTop = ($_GET['tt'] ?: 0) + 1;
+    $trimBottom = ($_GET['tb'] ?: 0) + 1;
+
     $sha256Url = hash_file('sha256', $url);
 
     header('Content-Type: text/plain;charset=UTF-8');
-    $shell = 'cat /var/www/db/' . $sha256Url . ' || curl -sS "' . $url . '" | /usr/lib/x86_64-linux-gnu/go/bin/ansize /dev/stdin /var/www/db/' . $sha256Url . ';';
+    $shell = "(cat /var/www/db/${sha256Url} || curl -sS '${url}' | /usr/lib/x86_64-linux-gnu/go/bin/ansize /dev/stdin /var/www/db/${sha256Url}) \
+        | sed 's/^\(\e[^\e]*\)\{${trimLeft}\}//; s/\(\e[^\e]*\)\{${trimRight}\}$//; ' \
+        | tail -n +${trimTop} \
+        | head -n -${trimBottom} \
+        ;
+    ";
     exec($shell, $output, $returnValue);
 
     foreach ($output as $row) {
