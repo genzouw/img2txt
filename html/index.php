4c32cae4231b39ba42f20077eb719571ee73d4dd
genzouw <genzouw@gmail.com>
2019-10-26 22:48:17 +0900

    Magnification and text can be specified arbitrarily.

--- html/index.php
+++ html/index.php
@@ -5,16 +5,26 @@ $dbDir = dirname(__DIR__) . '/db';
 $url = urldecode($_GET['url']);
 
 if (parse_url($url)) {
+    $width = $_GET['w'] ?? 100;
+    $char = $_GET['c'] ?? '0';
     $trimLeft = $_GET['tl'] ?? 0;
     $trimRight = $_GET['tr'] ?? 0;
     $trimTop = ($_GET['tt'] ?? 0) + 1;
     $trimBottom = ($_GET['tb'] ?? 0);
 
-    $sha256Url = hash_file('sha256', $url);
+    if ($width > 200) {
+        $width = 200;
+    }
+
+    $sha256Url = hash('sha256', "{$url}#{$width}");
 
     header('Content-Type: text/plain;charset=UTF-8');
-    $shell = "(cat ${dbDir}/${sha256Url} || curl  -A 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0' -sS '${url}' | ansize /dev/stdin ${dbDir}/${sha256Url}) \
+    $shell = "(
+        cat ${dbDir}/${sha256Url} \
+          || curl  -A 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0' -sS '${url}' | ansize /dev/stdin ${dbDir}/${sha256Url} ${width} \
+      ) \
         | sed 's/m1/m0/g' \
+        | sed 's/m0/m${char}/g' \
         | sed 's/^\(\e[^\e]*\)\{${trimLeft}\}//; s/\(\e[^\e]*\)\{${trimRight}\}$//; ' \
         | tail -n +${trimTop} \
         | head -n -${trimBottom} \
