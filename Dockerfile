91d7c28e2bf4f50c693539586ef43e4ea6dd9f65
genzouw <genzouw@gmail.com>
2019-10-26 21:09:41 +0900

    Print only 0 to output.

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,23 @@
-FROM genzouw/ansize:1.0.0
+FROM genzouw/ansize:1.0.2
 
 LABEL maintainer "genzouw <genzouw@gmail.com>"
 
-RUN sed -i 's@archive.ubuntu.com@ftp.jaist.ac.jp/pub/Linux@g' /etc/apt/sources.list
-
-ENV DEBIAN_FRONTEND=noninteractive
-
-RUN apt-get update \
-  && apt-get upgrade -y \
-  && apt-get -y install \
-    --no-install-recommends \
+RUN apk add \
+  --no-cache \
     apache2 \
     bash \
     curl \
-    locales \
-    php \
-    php-sqlite3 \
+    git \
+    openssl \
+    php7 \
+    php7-apache2 \
+    php7-iconv \
+    php7-json \
+    php7-openssl \
+    php7-phar \
+    tzdata \
     unzip \
-  && apt-get clean \
-  && rm -rf /var/cache/apt/archives/* /var/lib/apt/lists/*
+    ;
 
-ENV APACHE_RUN_USER www-data
-ENV APACHE_RUN_GROUP www-data
-ENV APACHE_PID_FILE /var/run/apache2.pid
-ENV APACHE_RUN_DIR /var/run/apache2
-ENV APACHE_LOG_DIR /var/log/apache2
-ENV APACHE_LOCK_DIR /var/lock/apache2
-
-ENTRYPOINT ["/usr/sbin/apache2"]
+ENTRYPOINT ["/usr/sbin/httpd"]
 CMD ["-D", "FOREGROUND"]
--- html/index.php
+++ html/index.php
@@ -1,17 +1,20 @@
 <?php
 
+$dbDir = dirname(__DIR__) . '/db';
+
 $url = urldecode($_GET['url']);
 
 if (parse_url($url)) {
-    $trimLeft = $_GET['tl'] ?: 0;
-    $trimRight = $_GET['tr'] ?: 0;
-    $trimTop = ($_GET['tt'] ?: 0) + 1;
-    $trimBottom = ($_GET['tb'] ?: 0);
+    $trimLeft = $_GET['tl'] ?? 0;
+    $trimRight = $_GET['tr'] ?? 0;
+    $trimTop = ($_GET['tt'] ?? 0) + 1;
+    $trimBottom = ($_GET['tb'] ?? 0);
 
     $sha256Url = hash_file('sha256', $url);
 
     header('Content-Type: text/plain;charset=UTF-8');
-    $shell = "(cat /var/www/db/${sha256Url} || curl  -A 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0' -sS '${url}' | /usr/lib/x86_64-linux-gnu/go/bin/ansize /dev/stdin /var/www/db/${sha256Url}) \
+    $shell = "(cat ${dbDir}/${sha256Url} || curl  -A 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0' -sS '${url}' | ansize /dev/stdin ${dbDir}/${sha256Url}) \
+        | sed 's/m1/m0/g' \
         | sed 's/^\(\e[^\e]*\)\{${trimLeft}\}//; s/\(\e[^\e]*\)\{${trimRight}\}$//; ' \
         | tail -n +${trimTop} \
         | head -n -${trimBottom} \
